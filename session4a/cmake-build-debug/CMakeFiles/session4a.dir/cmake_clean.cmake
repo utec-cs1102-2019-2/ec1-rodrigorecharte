2248ac9c4f7d4fcc12629c4bdb58e904f0325771
Jesus Edwin Bellido Angulo <jbellido@utec.edu.pe>
2019-09-11 16:56:16 -0500

    punteros

---
+++ session4a/cmake-build-debug/CMakeFiles/session4a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session4a.dir/main.cpp.obj"
+  "session4a.pdb"
+  "session4a.exe"
+  "session4a.exe.manifest"
+  "libsession4a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- session4a/main.cpp
+++ session4a/main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int ival = 42;
+    int *pival = &ival;
+    std::cout  << pival << "\t" << *pival << "\t" << &pival;
     return 0;
 }
