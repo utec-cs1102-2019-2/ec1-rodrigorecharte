a47eda04df5f628c597fd7be644e8b9383256d76
Rodrigo Recharte Alva <rodrigo.recharte@utec.edu.pe>
2019-09-12 10:53:52 -0500

    Ejercicio 3

--- session4b/ejercicio3.cpp
+++ session4b/ejercicio3.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    char carac = 'a';
+    int r = 0;
+    char *p = &carac;
+    char **pp_carac = &p;
+    int *p_entero = &r;
+    int **ptr_entero = &p_entero;
+    cout << "A caracter:\n" << *p << "\t" << p << "\n" << *pp_carac << "\t" << pp_carac << "\nA entero:\n" << *p_entero << "\t" << p_entero << "\n" << *ptr_entero << "\t" << ptr_entero;
 
     return 0;
 }
