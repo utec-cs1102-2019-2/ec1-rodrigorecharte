d6fde0d4f549814005eeda91ecf4e7af45a3aa0e
Rodrigo Recharte Alva <rodrigo.recharte@utec.edu.pe>
2019-10-10 11:08:22 -0500

    Session8b

---
+++ session8b/cmake-build-debug/CMakeFiles/session8b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session8b.dir/main.cpp.obj"
+  "session8b.pdb"
+  "session8b.exe"
+  "session8b.exe.manifest"
+  "session8b.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session8b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
